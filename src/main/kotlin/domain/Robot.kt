062edf58eb652bd79924551fd909b91e4735c482
Justin Crow <jcrowster@gmail.com>
2025-07-19 16:07:35 +0100

    Test Robot movement

--- src/main/kotlin/domain/Robot.kt
+++ src/main/kotlin/domain/Robot.kt
@@ -1,20 +1,39 @@
 package com.mindfulbytes.domain
 
-data class Robot(val heading: Heading) {
-    fun turn(turn: Movement): Robot {
+data class Robot(val heading: Heading, val coordinates: Coordinates) {
+    fun turn(turn: Turn): Robot {
         return when (turn) {
-            Movement.LEFT -> when (heading) {
+            Turn.LEFT -> when (heading) {
                 Heading.NORTH -> copy(heading = Heading.WEST)
                 Heading.SOUTH -> copy(heading = Heading.EAST)
                 Heading.EAST -> copy(heading = Heading.NORTH)
                 Heading.WEST -> copy(heading = Heading.SOUTH)
             }
-            Movement.RIGHT -> when (heading) {
+            Turn.RIGHT -> when (heading) {
                 Heading.NORTH -> copy(heading = Heading.EAST)
                 Heading.SOUTH -> copy(heading = Heading.WEST)
                 Heading.EAST -> copy(heading = Heading.SOUTH)
                 Heading.WEST -> copy(heading = Heading.NORTH)
             }
+        }
+    }
+
+    fun move(move: Move): Robot {
+        return when (move) {
+            Move.FORWARD -> {
+                copy(coordinates = coordinates.move(heading))
+            }
+        }
+    }
+}
+
+data class Coordinates(val x: Int, val y: Int) {
+    fun move(heading: Heading): Coordinates {
+        return when (heading) {
+            Heading.NORTH -> copy(y = y + 1)
+            Heading.SOUTH -> copy(y = y - 1)
+            Heading.EAST -> copy(x = x + 1)
+            Heading.WEST -> copy(x = x - 1)
         }
     }
 }
@@ -23,10 +42,15 @@ enum class Heading {
     NORTH,
     SOUTH,
     EAST,
-    WEST
+    WEST,
 }
 
-enum class Movement {
+enum class Turn {
     LEFT,
-    RIGHT
+    RIGHT,
 }
+
+enum class Move {
+    FORWARD,
+}
+
--- src/test/kotlin/domain/RobotTest.kt
+++ src/test/kotlin/domain/RobotTest.kt
@@ -1,19 +1,31 @@
 package domain
 
-import com.mindfulbytes.domain.Heading
-import com.mindfulbytes.domain.Movement
-import com.mindfulbytes.domain.Robot
+import com.mindfulbytes.domain.*
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class RobotTest {
 
     @Test
-    fun testRobotHeading() {
-        val robot = Robot(Heading.NORTH)
+    fun `should return new robot with correct heading after a turn`() {
+        val robot = Robot(Heading.NORTH, Coordinates(0, 0))
 
-        assertEquals(Heading.WEST, robot.turn(Movement.LEFT).heading)
-        assertEquals(Heading.SOUTH, robot.turn(Movement.RIGHT).turn(Movement.RIGHT).heading)
+        assertEquals(Heading.WEST, robot.turn(Turn.LEFT).heading)
+        assertEquals(Heading.SOUTH, robot.turn(Turn.RIGHT).turn(Turn.RIGHT).heading)
 
     }
+
+    @Test
+    fun `should return new robot with correct coordinates after a movement`() {
+        val robot = Robot(Heading.NORTH, Coordinates(0, 0))
+
+        assertEquals(robot.move(Move.FORWARD).coordinates, Coordinates(x = 0, y = 1))
+        assertEquals(
+            robot.move(Move.FORWARD)
+                .turn(Turn.RIGHT)
+                .move(Move.FORWARD)
+                .coordinates, Coordinates(x = 1, y = 1))
+
+    }
+
 }
